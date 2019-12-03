666ed0f6c1aa69e6ce080dce91af35f084ec1aa3
pseeth <prem@u.northwestern.edu>
2019-12-03 12:46:21 -0600

    adding max pooling to docstring

--- src/models.py
+++ src/models.py
@@ -60,6 +60,16 @@ class Dog_Classifier_Conv(nn.Module):
     - Output layer: a linear layer with one node per class (in this case 10)
 
     Activation function: ReLU for both hidden layers
+    
+    There should be a maxpool after each convolution. 
+    
+    The sequence of operations looks like this:
+    
+    	1. Apply convolutional layer with stride and kernel size specified
+		- note: uses hard-coded in_channels and out_channels
+		- read the problems to figure out what these should be!
+	2. Apply the activation function (ReLU)
+	3. Apply 2D max pooling with a kernel size of 2
 
     Inputs: 
     kernel_size: list of length 2 containing kernel sizes for the two convolutional layers
@@ -88,6 +98,16 @@ class Synth_Classifier(nn.Module):
     - Output layer: a linear layer with one node per class (in this case 2)
 
     Activation function: ReLU for both hidden layers
+    
+    There should be a maxpool after each convolution. 
+    
+    The sequence of operations looks like this:
+    
+    	1. Apply convolutional layer with stride and kernel size specified
+		- note: uses hard-coded in_channels and out_channels
+		- read the problems to figure out what these should be!
+	2. Apply the activation function (ReLU)
+	3. Apply 2D max pooling with a kernel size of 2
 
     Inputs: 
     kernel_size: list of length 3 containing kernel sizes for the three convolutional layers
